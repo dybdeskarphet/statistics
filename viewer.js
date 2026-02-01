ee8021678093611d2848de6065357fd373f68630
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2026-02-01 18:40:06 +0300

    Group notes and change viewer title

--- viewer.js
+++ viewer.js
@@ -7,6 +7,9 @@ document.addEventListener("DOMContentLoaded", function () {
     return;
   }
 
+  const imageName = imageSrc.split("/").pop();
+  document.title = `Viewer - ${imageName}`;
+
   var viewer = OpenSeadragon({
     id: "openseadragon1",
     prefixUrl: "",
@@ -19,13 +22,21 @@ document.addEventListener("DOMContentLoaded", function () {
     zoomPerScroll: 1.4,
   });
 
-  viewer.addHandler("open", function () {
+  function resetToCustomHome() {
+    // Safety check: make sure an item exists before calculating
+    if (viewer.world.getItemCount() === 0) return;
+
     var zoomLevel =
       viewer.viewport.getContainerSize().x /
       viewer.world.getItemAt(0).getContentSize().x;
+
     zoomLevel = viewer.viewport.imageToViewportZoom(zoomLevel);
+
+    // Zoom to the calculated level, centered at top-middle (0.5, 0)
     viewer.viewport.zoomTo(zoomLevel, new OpenSeadragon.Point(0.5, 0), true);
-  });
+  }
+
+  viewer.addHandler("open", resetToCustomHome);
 
   function bindAction(id, action) {
     const btn = document.getElementById(id);
@@ -42,9 +53,7 @@ document.addEventListener("DOMContentLoaded", function () {
     viewer.viewport.applyConstraints();
   });
 
-  bindAction("home", function () {
-    viewer.viewport.goHome();
-  });
+  bindAction("home", resetToCustomHome);
 
   bindAction("rotate", function () {
     var currentRotation = viewer.viewport.getRotation();
