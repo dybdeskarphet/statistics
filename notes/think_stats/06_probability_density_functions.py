dfec50d7fe17364f7498a3f313ac00d6e00135f9
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2026-02-25 04:09:46 +0300

    Add KDE plot chapter

--- notes/think_stats/06_probability_density_functions.py
+++ notes/think_stats/06_probability_density_functions.py
@@ -19,15 +19,19 @@ r"""
 """
 
 # %%
+from random import sample
 import matplotlib
+from matplotlib.lines import lineStyles
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import trimboth, norm
+from scipy.stats import f, trimboth, norm
 from scipy.integrate import simpson
 
 # %% [markdown]
 r"""
+## Normal distribution PDF
+
 We start with the PDF of the normal distribution. Formula of normal distribution PDF is:
 
 $$
@@ -87,3 +91,90 @@ We can also find the fraction by $F_X(2) - F_X(1)$
 
 # %%
 norm.cdf(2, trim_mean, trim_std) - norm.cdf(1, trim_mean, trim_std)
+
+# %% [markdown]
+r"""
+## Kernel Density Estimation (KDE)
+
+We can use KDE to estimate a PDF from the discrete data.
+"""
+
+# %%
+birth_weights = birth["totalwgt_kg"].dropna()
+trimmed_weight = pd.Series(trimboth(birth_weights, 0.02))
+
+# %%
+np.random.seed(3)
+n = 500
+sample_birth_weights = trimmed_weight.sample(n)
+sample_birth_weights
+
+# %%
+weight_kde_pmf = pd.Series(1 / n, index=sample_birth_weights)
+weight_kde_pmf
+len(weight_kde_pmf)
+
+# %% [markdown]
+r"""
+We can find the optimal $h$ using _Scott's rule of thumb_.
+
+$$
+h \approx 1.06 \cdot \hat{\sigma}n^{\frac{1}{5}}
+$$
+"""
+
+# %%
+x_vals = np.linspace(0, 7, 1000)
+h = 1.06 * sample_birth_weights.std() * len(sample_birth_weights) ** (-1 / 5)
+h
+
+# %%
+norm_dist = norm.pdf(
+    x_vals, loc=sample_birth_weights.mean(), scale=sample_birth_weights.std()
+)
+norm_pdf = pd.Series(norm_dist, index=x_vals)
+norm_pdf
+
+# %%
+fig, ax = plt.subplots(1, 3, figsize=(16, 4))
+ax[0].bar(weight_kde_pmf.index, weight_kde_pmf, width=0.01, color="red", alpha=0.6)
+kde_total = np.zeros_like(x_vals)
+for point in sample_birth_weights:
+    kernel = norm.pdf(x_vals, loc=point, scale=h) * (1 / n)
+    ax[1].plot(x_vals, kernel, color="gray", alpha=0.5, linestyle="-", linewidth=0.1)
+    kde_total += kernel
+
+ax[2].plot(norm_pdf.index, norm_pdf, color="blue", alpha=0.5)
+ax[2].plot(x_vals, kde_total, color="red")
+
+ax[0].set_xlabel("Weight")
+ax[0].set_ylabel(f"1/{n}")
+ax[0].set_title("First step - Data points for each weight")
+
+ax[1].set_title("Second Step - Normal distribution PDFs for each weight")
+
+ax[2].set_xlabel("Weight (kg)")
+ax[2].set_ylabel("Probability")
+ax[2].set_title("KDE plot of weights")
+
+
+plt.show()
+
+# %% [markdown]
+r"""
+We have our KDE plot. In some cases, like here where we have too many data points, it is better to use KDE plot instead of a histogram.
+
+We can calculate the area below our KDE plot to ensure that our KDE plot is also a PDF.
+"""
+
+# %%
+simpson(x=x_vals, y=kde_total)
+
+# %% [markdown]
+r"""
+## The Distribution Framework
+
+At this point, we have a complete set of ways to represent distributions: PMFs, CDFs and PDFs. Now, let's learn how to transform one to another.
+
+<img src="/notes/assets/distribution_framework.png" height="300"/>
+"""
